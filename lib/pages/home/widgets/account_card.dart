7a82a46aaa8c7f3e686d26dba96e4ce9369c0ed9
Nader Farhat <n-farhat@hotmail.com>
2021-03-01 19:29:13 -0300

    change card

--- lib/pages/home/widgets/account_card.dart
+++ lib/pages/home/widgets/account_card.dart
@@ -8,50 +8,77 @@ class AccountCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
-      color: Colors.black,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Icon(
-                Icons.monetization_on_outlined,
-              ),
-              Text(
-                'Conta',
-                style: TextStyle(fontSize: 12),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Saldo disponível',
-                style: TextStyle(fontSize: 12),
-              )
-            ],
-          ),
-          showData
-              ? Row(
-                  children: [
-                    Text(
-                      "R\$ 0,00",
-                      style: TextStyle(fontSize: 12),
-                    )
-                  ],
-                )
-              : Row(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: 42.0,
-                      child: const DecoratedBox(
-                        decoration: const BoxDecoration(color: Colors.red),
-                      ),
-                    )
-                  ],
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.white,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(6)),
+      ),
+      height: 160,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Icon(
+                    Icons.monetization_on_outlined,
+                    color: Color(0xff7d7d7d),
+                    size: 30.0,
+                  ),
                 ),
-        ],
+                Text(
+                  'Conta',
+                  style: TextStyle(fontSize: 18, color: Color(0xff7d7d7d)),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Saldo disponível',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff7d7d7d),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            showData
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "R\$ 0,00",
+                          style: TextStyle(
+                              fontSize: 36,
+                              color: Color(0xff151515),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  )
+                : Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: 42.0,
+                        child: const DecoratedBox(
+                          decoration:
+                              const BoxDecoration(color: Color(0xfff5f5f5)),
+                        ),
+                      )
+                    ],
+                  ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/home/widgets/credit_card.dart
+++ lib/pages/home/widgets/credit_card.dart
@@ -8,74 +8,107 @@ class CreditCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 140,
-      color: Colors.black,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Icon(
-                Icons.credit_card,
-              ),
-              Text(
-                'Cartão de Crédito',
-                style: TextStyle(fontSize: 12),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Fatura atual',
-                style: TextStyle(fontSize: 12),
-              )
-            ],
-          ),
-          showData
-              ? Row(
-                  children: [
-                    Text(
-                      "R\$ 0,00",
-                      style: TextStyle(fontSize: 12),
-                    )
-                  ],
-                )
-              : Row(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: 22.0,
-                      child: const DecoratedBox(
-                        decoration: const BoxDecoration(color: Colors.red),
-                      ),
-                    )
-                  ],
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.white,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(6)),
+      ),
+      height: 160,
+      // color: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Icon(
+                    Icons.credit_card,
+                    color: Color(0xff7d7d7d),
+                    size: 30.0,
+                  ),
                 ),
-          showData
-              ? Row(
-                  children: [
-                    Text(
-                      "Limite disponível ",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    Text(
-                      "R\$ 3000,00",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ],
+                Text(
+                  'Cartão de Crédito',
+                  style: TextStyle(fontSize: 18, color: Color(0xff7d7d7d)),
                 )
-              : Row(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: 22.0,
-                      child: const DecoratedBox(
-                        decoration: const BoxDecoration(color: Colors.red),
-                      ),
-                    )
-                  ],
-                ),
-        ],
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Fatura atual',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff7d7d7d),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            showData
+                ? Row(
+                    children: [
+                      Text(
+                        "R\$ 0,00",
+                        style: TextStyle(
+                            fontSize: 32,
+                            color: Color(0xff24a7ed),
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                : Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: 22.0,
+                        child: const DecoratedBox(
+                          decoration:
+                              const BoxDecoration(color: Color(0xfff5f5f5)),
+                        ),
+                      )
+                    ],
+                  ),
+            showData
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Limite disponível ",
+                          style:
+                              TextStyle(fontSize: 12, color: Color(0xff747474)),
+                        ),
+                        Text(
+                          "R\$ 3000,00",
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Color(0xff125d36),
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                : Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: 34.0,
+                        child: const DecoratedBox(
+                          decoration:
+                              const BoxDecoration(color: Color(0xfff5f5f5)),
+                        ),
+                      )
+                    ],
+                  ),
+          ],
+        ),
       ),
     );
   }
